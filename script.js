ff108966b65c579618171f653ebdf28a9cb0dc79
Babylyn <190585086+m1ra-san@users.noreply.github.com>
2025-08-20 21:09:43 +0800

    Create display function and add more style for the cards

--- script.js
+++ script.js
@@ -1,32 +1,55 @@
 const myLibrary = [
-    {title:"Game of thrones:",
-        author:"Goerge RR",
-        pages:694,
-    },    {title:"Game of thrones:",
-        author:"Goerge RR",
-        pages:694,
-    },    {title:"sdfafasfafsd",
-        author:"asfafsaa",
-        pages:694,
-    }
+  {
+    title: "Game of thrones:",
+    author: "Goerge RR",
+    pages: 694,
+  }, {
+    title: "Game of thrones:",
+    author: "Goerge RR",
+    pages: 694,
+  }, {
+    title: "sdfafasfafsd",
+    author: "asfafsaa",
+    pages: 694,
+  }
 ];
 
 
-function Book(title,author,pages) {
-  this.title=title,
-  this.author=author,
-  this.pages=pages,
-  this.bookId = crypto.randomUUID();
+function Book(title, author, pages) {
+  this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.bookId = crypto.randomUUID();
+    this.info=function(){
+      return "The principal story chronicles the power struggle for the Iron Throne among the great Houses of Westeros following the death of King Robert in A Game of Thrones."
+    }
 }
 
 function addBookToLibrary() {
   return myLibrary.map(item => new Book(item.title, item.author, item.pages));
 }
 
-function display(books){
+function display(books) {
+  const library = document.querySelector(".library")
+  library.innerHTML = ""///clear div.library 
 
+  books.forEach(book => {
+    const card = document.createElement("div");
+    card.classList.add("card")
+
+    card.innerHTML =
+      `<h2>Title: ${book.title}</h2>
+      <p>Author: ${book.author} </p>
+      <div class="description">
+      <p>Pages: ${book.info()}</p>
+      <p>Pages: ${book.pages}</p>
+      </div>`
+
+    library.appendChild(card)
+  });
 }
 
 
-const books = addBookToLibrary();
+const books = addBookToLibrary(myLibrary);
+display(books)
 console.log(books)

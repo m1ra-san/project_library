a7798a165494a0cab5f8e54053c7004711e31df7
Babylyn <190585086+m1ra-san@users.noreply.github.com>
2025-08-21 12:53:28 +0800

    Add delete and read/unread functionality

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ function Book(title, author, pages) {
     this.author = author,
     this.pages = pages,
     this.bookId = crypto.randomUUID();
-    this.info=function(){
+  this.isRead = false,
+    this.info = function () {
       return "The principal story chronicles the power struggle for the Iron Throne among the great Houses of Westeros following the death of King Robert in A Game of Thrones."
     }
 }
@@ -36,6 +37,8 @@ function display(books) {
   books.forEach(book => {
     const card = document.createElement("div");
     card.classList.add("card")
+    card.setAttribute("data-index", book.bookId)
+    card.setAttribute("value", book.isRead)
 
     card.innerHTML =
       `<h2>Title: ${book.title}</h2>
@@ -43,11 +46,37 @@ function display(books) {
       <div class="description">
       <p>Pages: ${book.info()}</p>
       <p>Pages: ${book.pages}</p>
+      </div>
+      <div>
+      <button class="delete">Delete</button>
+      <button class="read">Read</button>
       </div>`
+
+    card.querySelector(".delete").addEventListener("click", (e) => {
+      // card.remove();
+      const idx = e.target.getAttribute("data-index");
+      books.splice(idx, 1);
+      display(books);
+    });
+
+    card.querySelector(".read").addEventListener("click", (e) => {
+      const cardElement = e.target.closest(".card");
+
+      //Toggle
+      book.isRead=!book.isRead
+      console.log(book.isRead)
+      console.log(books)
+
+      e.target.textContent =book.isRead? "Unread" : "Read";
+      cardElement.setAttribute("value",book.isRead)
+    })
 
     library.appendChild(card)
   });
 }
+
+
+
 
 
 const books = addBookToLibrary(myLibrary);
